e07a703a6de981ae0de90c1789c4d0d7a649c360
ediva <ediva@192.168.15.2>
2026-02-16 15:47:58 -0300

    Primeiro commit

---
+++ FiapProject/src/Fase05/VariaveisExemplo.java
@@ -0,0 +1,16 @@
+package Fase05;
+
+public class VariaveisExemplo {
+	public static void main(String[] args) {
+		int idade = 30;
+		double altura = 1.75;
+		char genero = 'M';
+		boolean enEstudante = true;
+		
+		System.out.println("Idade:" + idade);
+		System.out.println("altura:" + altura);
+		System.out.println("genero:" + genero);
+		System.out.println("Estudante:" + enEstudante);
+		
+	}
+}
